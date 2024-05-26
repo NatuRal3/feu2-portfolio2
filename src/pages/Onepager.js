1db7112d42c172ea1953c8b1ab8ad3155880b063
Marius Agerup <Marius.schei@outlook.com>
2024-05-26 22:48:05 +0200

    last commit for my education

--- src/pages/Onepager.js
+++ src/pages/Onepager.js
@@ -7,36 +7,41 @@ import projectExamImage from "../images/exam-2.png";
 function Onepager() {
   return (
     <div>
+      <div className="cont name"></div>
       <h1>Marius Agerup Schei</h1>
 
-      <div id="cont intro">
+      <div className="cont intro">
         <p>Welcome to my simple portfolio page</p>
-        <p>Currently working as a business application consultant and developer</p>
+        <p>Currently working as a business application consultant and inhouse developer</p>
+      </div>
+      <div className="cont contact">
+        <h2>Contact</h2>
+        <div>
+          <a href="https://github.com/NatuRal3" target="_blank" rel="noopener noreferrer">
+            <i class="bi bi-github"> /NatuRal3</i>
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/mariusschei/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="bi bi-linkedin"> /mariusschei</i>
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://discord.com/users/318457989440733184"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="bi bi-discord"> /natural8976</i>
+          </a>
+        </div>
       </div>
 
-      <div id="cont Skills">
-        <h2>
-          <i class="bi bi-code">
-            Skills <i class="bi bi-code-slash"></i>
-          </i>
-        </h2>
-        <i class="bi bi-filetype-html"></i>
-        <i class="bi bi-filetype-css"></i>
-        <i class="bi bi-filetype-scss"></i>
-        <i class="bi bi-filetype-md"></i>
-        <i class="bi bi-filetype-js"></i>
-        <i class="bi bi-filetype-tsx"></i>
-        <i class="bi bi-filetype-jsx"></i>
-        <i class="bi bi-filetype-yml"></i>
-        <i class="bi bi-filetype-json"></i>
-        <i class="bi bi-filetype-xml"></i>
-        <i class="bi bi-git"></i>
-        <i class="bi bi-wordpress"></i>
-
-        <p>Adobe XD - Cypress - API</p>
-      </div>
-
-      <div id="cont projects">
+      <div className="cont projects">
         <h2>Projects</h2>
         <Card_
           image={semesterProjectImage}
@@ -59,11 +64,6 @@ function Onepager() {
           github="https://github.com/Noroff-FEU-Assignments/project-exam-2-NatuRal3"
           visit="https://marius-exam2.netlify.app"
         />
-      </div>
-
-      <div id="contact">
-        <h2>Contact</h2>
-        <p>LinkedIn, Github, Discord</p>
       </div>
     </div>
   );
